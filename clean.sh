5e84f99600e9640f82233ab0a552b9cf3e9c0edf
naisanza <naisanza@gmail.com>
2026-06-17 00:59:38 -0700

    clean script

---
+++ clean.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+# clean all volumes
+
+image="openclaw-agent"
+
+set -xe
+
+docker volume rm $image-agents
+docker volume rm $image-gateway
+docker volume rm $image-workspace
+docker volume rm $image-memory
+docker volume rm $image-state
+docker volume rm $image-pi
+docker volume rm $image-ssh
